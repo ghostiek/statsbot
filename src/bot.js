26ceab4f2b553fe3b475c69c59826e3286dcf4f2
Patrick Tawil <patricktawil1@hotmail.com>
2019-03-27 21:43:21 +0800

    fixed indentation

--- src/bot.js
+++ src/bot.js
@@ -17,7 +17,7 @@ client.commands = new Discord.Collection()
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for(const file of commandFiles){
+for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.name, command);
 }
@@ -32,40 +32,40 @@ client.on('ready', () => {
 //Listening to messages to trigger commands
 client.on('message', message => {
 
-//Ignores messages without prefix or bot messages
-if (!message.content.startsWith(prefix) || message.author.bot) return;
+  //Ignores messages without prefix or bot messages
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).split(/ +/);
-const commandName = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
-//Ignore invalid commands
-if (!client.commands.has(commandName)) return;
+  //Ignore invalid commands
+  if (!client.commands.has(commandName)) return;
 
-const command = client.commands.get(commandName);
+  const command = client.commands.get(commandName);
 
-//Verifies if the command was sent as a DM or was executed on the server
-if (command.guildOnly && message.channel.type !== 'text') {
-	return message.reply('I can\'t execute that command inside DMs!');
-}
-
-//Inform user that his command was incorrectly formatted
-if (command.args && args.length < command.minArgs) {
-  let reply = `You didn't provide enough arguments, ${message.author}!`;
-  
-  //Explains format if defined
-  if (command.usage){
-    reply += `\nThe proper usage of this function would be \`${prefix}${command.name} ${command.usage}\``;
+  //Verifies if the command was sent as a DM or was executed on the server
+  if (command.guildOnly && message.channel.type !== 'text') {
+    return message.reply('I can\'t execute that command inside DMs!');
   }
 
-  return message.channel.send(reply);
-}
+  //Inform user that his command was incorrectly formatted
+  if (command.args && args.length < command.minArgs) {
+    let reply = `You didn't provide enough arguments, ${message.author}!`;
 
-try {
-  command.execute(message, args);
-} catch (error) {
-  console.error(error);
-  message.reply('There was an error trying to execute that command!');
-}
+    //Explains format if defined
+    if (command.usage) {
+      reply += `\nThe proper usage of this function would be \`${prefix}${command.name} ${command.usage}\``;
+    }
+
+    return message.channel.send(reply);
+  }
+
+  try {
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('There was an error trying to execute that command!');
+  }
 });
 
 
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'ping',
     execute(message, args){
         if (message.member.hasPermission(checkAdmin = true)){
-            message.channel.send(`Pong Admin`);
+            message.channel.send('Pong Admin');
         } else{
             message.channel.send('Pong');
         }
